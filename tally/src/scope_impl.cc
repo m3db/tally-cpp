e3ec90f1bcc938984320b3ead109d6fa4c66d764
Jerome Froelich <jeromefroe@uber.com>
2018-07-16 14:05:48 -0400

    Fix initialization error

--- tally/src/scope_impl.cc
+++ tally/src/scope_impl.cc
@@ -109,7 +109,8 @@ std::shared_ptr<tally::Histogram> ScopeImpl::Histogram(const std::string &name,
 }
 
 std::shared_ptr<tally::Scope> ScopeImpl::SubScope(const std::string &name) {
-  return SubScope(FullyQualifiedName(name), {});
+  std::unordered_map<std::string, std::string> empty_tags = {};
+  return SubScope(FullyQualifiedName(name), empty_tags);
 }
 
 std::shared_ptr<tally::Scope> ScopeImpl::Tagged(
